a425b29af156319039eacb603d567bc04a3c7b62
abdulbari22385 <125637321+abdulbari22385@users.noreply.github.com>
2023-02-17 12:44:52 +0500

    Create Question No. 10.ts

---
+++ Question No. 10.ts
@@ -0,0 +1,5 @@
+//This program shows you that how to print hello with your friend's name as variable.
+let friends name = "Abdullah";
+
+let message = "Hello ${friends name}" ;
+console.log(message);
