98f6adfd26c5326d85981c9d451f53be4429316a
abdulbari22385 <125637321+abdulbari22385@users.noreply.github.com>
2023-02-17 19:43:34 +0500

    Create Question No. 20.ts

---
+++ Question No. 20.ts
@@ -0,0 +1,6 @@
+let favoriteFruits: string[] = ["apple", "banana", "orange", "kiwi", "mango"];
+
+console.log("My favorite fruits are:");
+for(let i = 0; i < favoriteFruits.length; i++) {
+  console.log("- " + favoriteFruits[i]);
+}
