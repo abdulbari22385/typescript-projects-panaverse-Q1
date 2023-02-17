28c025edc38c27c19fc8f800f2ce544a047aa9c9
abdulbari22385 <125637321+abdulbari22385@users.noreply.github.com>
2023-02-17 20:13:33 +0500

    Create Question No. 25.ts

---
+++ Question No. 25.ts
@@ -0,0 +1,11 @@
+let alien_color: string = 'green';
+
+// Passes the if test
+if (alien_color === 'green') {
+  console.log("The player just earned 5 points!");
+}
+
+// Fails the if test
+if (alien_color !== 'green') {
+  // no output
+}
