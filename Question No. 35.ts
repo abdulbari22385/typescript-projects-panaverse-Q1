364dd6e26017c62542aac8f28741ba6e77ab4a68
abdulbari22385 <125637321+abdulbari22385@users.noreply.github.com>
2023-02-17 20:59:59 +0500

    Create Question No. 35.ts

---
+++ Question No. 35.ts
@@ -0,0 +1,14 @@
+const animals: string[] = ["dog", "cat", "rabbit"];
+
+// Print the names of the animals
+for (const animal of animals) {
+  console.log(animal);
+}
+
+// Print a statement about each animal
+for (const animal of animals) {
+  console.log(`A ${animal} would make a great pet.`);
+}
+
+// Print what these animals have in common
+console.log("Any of these animals would make a great pet!");
