e7350fa3ac2dbb7640f81b1b3953c8b1a6f6f09c
abdulbari22385 <125637321+abdulbari22385@users.noreply.github.com>
2023-02-17 12:12:45 +0500

    Create Question No. 05.ts

---
+++ Question No. 05.ts
@@ -0,0 +1,5 @@
+let famous_person: string = "Allama Iqbal";
+let quote: string = "The wing of the Falcon brings to the king, the wing if the crow brings him to the cemetery.";
+let message: string = `${famous_person} once said, "${quote}"`;
+
+console.log(message);
