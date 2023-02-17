f9a67a9a0c62c664148a658291cee7e76041052b
abdulbari22385 <125637321+abdulbari22385@users.noreply.github.com>
2023-02-17 12:10:11 +0500

    Create Question No. 04.ts

---
+++ Question No. 04.ts
@@ -0,0 +1,4 @@
+let author: string = "Allama Iqbal";
+let quote: string = "The wing of the Falcon brings to the king, the wing if the crow brings him to the cemetery.";
+
+console.log(`${author} once said, "${quote}"`);
