c07a7700d0b162901cdc6ea869ca4f08c1170c0f
abdulbari22385 <125637321+abdulbari22385@users.noreply.github.com>
2023-02-17 12:25:57 +0500

    Create Question no. 08.ts

---
+++ Question no. 08.ts
@@ -0,0 +1,12 @@
+let sum: number = 5 + 3;
+console.log(`${sum}`);
+
+let difference: number = 10 - 2;
+console.log(`${difference}`);
+
+let product: number = 4 * 2;
+console.log(`${product}`);
+
+let quotient: number = 16 / 2;
+console.log(`${quotient}`);
+This code demonstrates four basic arithme
