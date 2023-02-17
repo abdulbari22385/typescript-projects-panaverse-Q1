724a1a744bf8c51ce15f8192d1e4f17e614dab31
abdulbari22385 <125637321+abdulbari22385@users.noreply.github.com>
2023-02-17 20:51:03 +0500

    Create Question No. 30.ts

---
+++ Question No. 30.ts
@@ -0,0 +1,9 @@
+let usernames: string[] = ['Ali', 'Ahmed', 'Abdullah', 'Ali', 'Admin'];
+
+for (let username of usernames) {
+  if (username === 'Admin') {
+    console.log('Hello admin, would you like to see a status report?');
+  } else {
+    console.log(`Hello ${username}, thank you for logging in again.`);
+  }
+}
