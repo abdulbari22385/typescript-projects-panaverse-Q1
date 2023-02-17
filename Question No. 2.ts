e1c10f6c2f863114386e737dcb939cbbd5b5729d
abdulbari22385 <125637321+abdulbari22385@users.noreply.github.com>
2023-02-17 11:40:34 +0500

    Update Question No. 2.ts

--- Question No. 2.ts
+++ Question No. 2.ts
@@ -1,8 +1,3 @@
-// Store the person's name in a variable
 let name: string = "Abdul";
-
-// Create the personalized message
 let message: string = `Hello ${name}, would you like to learn some Python today?`;
-
-// Print the message to the console
 console.log(message);
