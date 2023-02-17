3624f319d1428063140da95bf1b257ecf8cdad20
abdulbari22385 <125637321+abdulbari22385@users.noreply.github.com>
2023-02-17 12:20:49 +0500

    Create Question no. 07.ts

---
+++ Question no. 07.ts
@@ -0,0 +1,11 @@
+let sum: number = 5 + 3;
+console.log(`5 + 3 = ${sum}`);
+
+let difference: number = 10 - 2;
+console.log(`10 - 2 = ${difference}`);
+
+let product: number = 4 * 2;
+console.log(`4 * 2 = ${product}`);
+
+let quotient: number = 16 / 2;
+console.log(`16 / 2 = ${quotient}`);
