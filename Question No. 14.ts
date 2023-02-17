f8ea5be8dbb057f64ee618030ef073c3dcbd7184
abdulbari22385 <125637321+abdulbari22385@users.noreply.github.com>
2023-02-17 12:57:44 +0500

    Create Question No. 14.ts

---
+++ Question No. 14.ts
@@ -0,0 +1,5 @@
+let guests: string[] = ["Abdullah", "Ahmed", "Ali"];
+
+for (let i = 0; i < guests.length; i++) {
+  console.log(`Dear ${guests[i]},\n\nYou are cordially invited to a dinner party at my home on Saturday, March 5th at 7:00 pm. We would be honored if you could attend and share your insights with us. Please let us know if you can make it.\n\nBest regards,\nAbdul Bari`);
+}
