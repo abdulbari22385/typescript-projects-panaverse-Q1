6c3ab9f248864fda2557e9a8ce50d0b4313fb724
abdulbari22385 <125637321+abdulbari22385@users.noreply.github.com>
2023-02-17 20:45:38 +0500

    Create Question No. 26.ts

---
+++ Question No. 26.ts
@@ -0,0 +1,16 @@
+let alien_color: string = "yellow";
+
+if (alien_color === "green") {
+  console.log("You earned 5 points for shooting the green alien!");
+} else {
+  console.log("You earned 10 points for shooting the non-green alien!");
+}
+
+//we change the alien color into green
+let alien_color: string = "green";
+
+if (alien_color === "green") {
+  console.log("You earned 5 points for shooting the green alien!");
+} else {
+  console.log("You earned 10 points for shooting the non-green alien!");
+}
