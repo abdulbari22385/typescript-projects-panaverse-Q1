5fa82cb6f96e62641f5bb32e43ee0cba116c6720
abdulbari22385 <125637321+abdulbari22385@users.noreply.github.com>
2023-02-17 12:50:29 +0500

    Create Question No. 12.ts

---
+++ Question No. 12.ts
@@ -0,0 +1,5 @@
+let names: string[] = ["Abdullah", "Badar", "Umar", "Daood", "Ehtisham"];
+
+for (let i = 0; i < names.length; i++) {
+  console.log(`Hello ${names[i]}, how are you doing today?`);
+}
