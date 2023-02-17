da157f4ac47680d79fff71f61c657a45344f1464
abdulbari22385 <125637321+abdulbari22385@users.noreply.github.com>
2023-02-17 19:37:50 +0500

    Create Question No. 19.ts

---
+++ Question No. 19.ts
@@ -0,0 +1,7 @@
+let guestList = ['Ahmed', 'Abdullah', 'Ali'];
+
+for (let guest of guestList) {
+  console.log(`Hi ${guest}, please come to my dinner party!`);
+}
+
+console.log(`I am inviting ${guestList.length} people to dinner.`);
