091afdb0aab725f634070000e926521848fadd94
abdulbari22385 <125637321+abdulbari22385@users.noreply.github.com>
2023-02-17 12:18:12 +0500

    Create Question No. 06.ts

---
+++ Question No. 06.ts
@@ -0,0 +1,5 @@
+let name: string = "\t\n   Abdul Bari    \n\t";
+console.log(`Name with whitespace: "${name}"`);
+
+let strippedName: string = name.trim();
+console.log(`Name without whitespace: "${strippedName}"`);
