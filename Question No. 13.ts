e7d1279a4594b7e223577dd3ae4597c7a92e41cf
abdulbari22385 <125637321+abdulbari22385@users.noreply.github.com>
2023-02-17 12:54:50 +0500

    Create Question No. 13.ts

---
+++ Question No. 13.ts
@@ -0,0 +1,5 @@
+let transportation: string[] = ["Honda Civic", "Honda City", "Honda YBR", "Mercedes Benz"];
+
+for (let i = 0; i < transportation.length; i++) {
+  console.log(`I would like to drive a ${transportation[i]}.`);
+}
