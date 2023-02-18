9e65eed985617954aebd4a603b2508f6f9b201bd
Abdul Bari <125637321+abdulbari22385@users.noreply.github.com>
2023-02-18 10:47:44 +0500

    Create Question No. 41.ts

---
+++ Question No. 41.ts
@@ -0,0 +1,7 @@
+function show_magicians(magicians: string[]): void {
+  magicians.forEach(magician => console.log(magician));
+}
+
+const magicians: string[] = ["David Copperfield", "Harry Houdini", "Penn & Teller", "Derren Brown"];
+
+show_magicians(magicians);
